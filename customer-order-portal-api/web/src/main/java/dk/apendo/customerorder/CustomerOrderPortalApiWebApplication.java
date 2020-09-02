f09a1ac1074359103d7e07f9a61e64b5b5d6e40d
Victor Nordstrand <victor.nordstrand@apendo.dk>
2020-09-02 23:01:44 +0200

    first commit

---
+++ customer-order-portal-api/web/src/main/java/dk/apendo/customerorder/CustomerOrderPortalApiWebApplication.java
@@ -0,0 +1,12 @@
+package dk.apendo.customerorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerOrderPortalApiWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerOrderPortalApiWebApplication.class, args);
+    }
+}
